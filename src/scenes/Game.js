62b2ac8c80c3c95591f91ca30fb46222d3eb58a7
Gloomsfield <amory@acosta.boo>
2026-03-11 10:25:30 -0700

    have camera follow the player

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,6 +6,8 @@ class Game extends Phaser.Scene {
 	create() {
 		this.player = new Player(this, { x: 5, y: 5 });
 		this.gift = new DebugGift(this, { x: 0, y: 0 });
+
+		this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
 	}
 
 	update() {
